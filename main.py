c015698c7cf689c33a09a1e2dbab425054fb362f
Sydney McGee <mcgee.sydney@gmail.com>
2023-08-14 22:50:29 -0500

    Added functionality using focus method to have email already available + added functionality to add stored password to file data_text and removed currently displayed info.

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window =Tk()
@@ -27,8 +40,10 @@ password_label.grid(row=3, column=0)
 
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "bealch44@fake.com")
 password_entry = Entry(width=18)
 password_entry.grid(row=3, column=1)
 
@@ -37,7 +52,7 @@ password_entry.grid(row=3, column=1)
 
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=15)
+add_button = Button(text="Add", width=15, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
